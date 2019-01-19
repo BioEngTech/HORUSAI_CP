689dc4d6c6638acbc5ecfecc689f3468ea8a1445
luis gois <luismanuelgois@hotmail.com>
2019-01-19 14:07:05 +0100

    new features to do

---
+++ masterAPP/app/src/main/java/vigi/patient/user/main.java
@@ -0,0 +1,49 @@
+package vigi.patient.user;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import vigi.patient.R;
+
+public class main extends AppCompatActivity implements View.OnClickListener{
+
+    private Button featOne;
+    private Button next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_main);
+
+        featOne = findViewById(R.id.featureOne);
+
+        next = findViewById(R.id.next);
+
+        featOne.setOnClickListener(this);
+        next.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+        if (view.getId() == featOne.getId()) {
+
+
+            // TODO execute function and return Appointments --> getAppointments(patientId, day, hours)
+
+
+        } else if (view.getId() == next.getId()) {
+
+            Intent launchUserIntent = new Intent(main.this,chooseTreatment.class);
+            startActivity(launchUserIntent);
+            finish();
+        }
+    }
+}
